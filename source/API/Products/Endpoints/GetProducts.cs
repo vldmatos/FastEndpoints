b954481a0a3d5512e64e3c1961bb8cb880bf1def
Vladimir Matos <vladimir.matos@outlook.com>
2024-06-20 17:24:30 -0300

    feat: get products

---
+++ source/API/Products/Endpoints/GetProducts.cs
@@ -0,0 +1,22 @@
+﻿using Configurations.Data;
+using Configurations.Models;
+using FastEndpoints;
+
+namespace API.Products.Endpoints
+{
+    public class GetProducts(DataContext dataContext) : EndpointWithoutRequest<IEnumerable<Product>>
+    {
+        public override void Configure()
+        {
+            Get("products");
+            AllowAnonymous();
+        }
+
+        public override Task HandleAsync(CancellationToken cancelationToken)
+        {
+            Response = [.. dataContext.Products];
+
+            return Task.CompletedTask;
+        }
+    }
+}
